953e623817f44dd21e812301dd05990eea9f072f
avtobys <48291601+avtobys@users.noreply.github.com>
2021-05-21 03:08:24 +0300

    main

---
+++ commit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SSH_PORT=22
+SSH_USER=root
+SSH_SERVER=192.168.56.111
+SSH_PATH=/var/www/user/public_html
+SSH_OG=username:groupname # recursive owner:group from chown -R SSH_PATH
+
+
+rsync -avzhP -e "ssh -p $SSH_PORT" --exclude=.vscode --exclude=inc/db.php --exclude=commit.sh ./ "$SSH_USER@$SSH_SERVER:$SSH_PATH"
+ssh -p $SSH_PORT "$SSH_USER@$SSH_SERVER" "chown -R $SSH_OG $SSH_PATH"
