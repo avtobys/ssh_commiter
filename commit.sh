913d37c2991202eddbfe127b93daea0e13f55f21
avtobys <48291601+avtobys@users.noreply.github.com>
2021-05-21 06:16:46 +0300

    add rsa key

--- commit.sh
+++ commit.sh
@@ -5,7 +5,12 @@ SSH_USER=root
 SSH_SERVER=192.168.56.111
 SSH_PATH=/var/www/user/public_html
 SSH_OG=username:groupname # recursive owner:group from chown -R SSH_PATH
+RSA_PATH=/c/Users/Username/.ssh/id_rsa # your rsa key
 
+if [[ -z "$(ssh-add -l | grep -i rsa)" ]]; then
+    eval `ssh-agent`
+    ssh-add $RSA_PATH
+fi
 
 rsync -avzhP -e "ssh -p $SSH_PORT" --exclude=.vscode --exclude=inc/db.php --exclude=commit.sh ./ "$SSH_USER@$SSH_SERVER:$SSH_PATH"
 ssh -p $SSH_PORT "$SSH_USER@$SSH_SERVER" "chown -R $SSH_OG $SSH_PATH"
